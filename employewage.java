004cf335d3d68d38f6a9c8958c22943b5bd39c6d
Your Name <you@example.com>
2021-04-27 19:21:54 +0530

    employee present or not

--- employewage.java
+++ employewage.java
@@ -1,9 +1,15 @@
+import java.util.*;
 public class EmployeWageComputation
 {  
    public static void main (String[] args)
    {
       System.out.println("Welcome to Employee Wage Computation program on master Branch");
-
+       
+        double check = Math.floor(Math.random() * 10) % 2;
+        if ( check == 1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
    }
 
 
