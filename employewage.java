91f958014cacd796d000ab7851ebdf3d09aaaf25
Your Name <you@example.com>
2021-04-27 18:48:22 +0530

    first commit from master branch about welcome

---
+++ employewage.java
@@ -0,0 +1,17 @@
+public class EmployeWageComputation
+{  
+   public static void main (String[] args)
+   {
+      System.out.println("Welcome to Employee Wage Computation program on master Branch");
+
+   }
+
+
+
+
+
+
+
+
+
+} 
